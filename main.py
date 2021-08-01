03d87bfa5feed65a78fa4f3ac358c07bbfd8f783
professorcode1 <raghkum2000@gmail.com>
2021-08-02 02:07:20 +0530

    ability to add edges to tree via player moves added. Game moves remaining

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 import time
-
+from copy import deepcopy
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -12,10 +12,104 @@ from selenium.webdriver.common.action_chains import ActionChains
 depth_of_expectimax = 6
 
 
+def print_board(board):
+    for i in range(4):
+        print(board[i])
+    print("\n\n")
+
+
+def create_up_board(board):
+    up_board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    for j in range(4):
+        hlpr_arr = []
+        for i in range(4):
+            if board[i][j]:
+                hlpr_arr.append(board[i][j])
+        # print(hlpr_arr)
+        for i in range(0, len(hlpr_arr) - 1):
+            # print(hlpr_arr[i], hlpr_arr[i+1], hlpr_arr[i] == hlpr_arr[i+1])
+            if hlpr_arr[i] == hlpr_arr[i + 1] and hlpr_arr[i]:
+                hlpr_arr[i] += 1
+                for x in range(i + 1, len(hlpr_arr) - 1):
+                    hlpr_arr[x] = hlpr_arr[x + 1]
+                hlpr_arr[len(hlpr_arr) - 1] = 0
+        for i in range(len(hlpr_arr)):
+            up_board[i][j] = hlpr_arr[i]
+    return up_board
+
+
+def create_left_board(board):
+    left_board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    for i in range(4):
+        hlpr_arr = []
+        for j in range(4):
+            if board[i][j]:
+                hlpr_arr.append(board[i][j])
+        # print(hlpr_arr)
+        for j in range(0, len(hlpr_arr) - 1):
+            # print(hlpr_arr[i], hlpr_arr[i+1], hlpr_arr[i] == hlpr_arr[i+1])
+            if hlpr_arr[j] == hlpr_arr[j + 1] and hlpr_arr[j]:
+                hlpr_arr[j] += 1
+                for x in range(j + 1, len(hlpr_arr) - 1):
+                    hlpr_arr[x] = hlpr_arr[x + 1]
+                hlpr_arr[len(hlpr_arr) - 1] = 0
+        for j in range(len(hlpr_arr)):
+            left_board[i][j] = hlpr_arr[j]
+    return left_board
+
+
+def create_right_board(board):
+    right_board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    for i in range(4):
+        hlpr_arr = []
+        for j in range(4):
+            if board[i][j]:
+                hlpr_arr.append(board[i][j])
+        # print(hlpr_arr)
+        for j in range(len(hlpr_arr) - 1, 0, -1):
+            # print(hlpr_arr[i], hlpr_arr[i+1], hlpr_arr[i] == hlpr_arr[i+1])
+            if hlpr_arr[j] == hlpr_arr[j - 1] and hlpr_arr[j]:
+                hlpr_arr[j] += 1
+                for x in range(j - 1, 0, -1):
+                    hlpr_arr[x] = hlpr_arr[x - 1]
+                hlpr_arr[0] = 0
+        for j in range(len(hlpr_arr)):
+            right_board[i][4 - len(hlpr_arr) + j] = hlpr_arr[j]
+    return right_board
+
+
+def create_down_board(board):
+    down_board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    for j in range(4):
+        hlpr_arr = []
+        for i in range(4):
+            if board[i][j]:
+                hlpr_arr.append(board[i][j])
+        # print(hlpr_arr)
+        for i in range(len(hlpr_arr) - 1, 0, -1):
+            # print(hlpr_arr[i], hlpr_arr[i+1], hlpr_arr[i] == hlpr_arr[i+1])
+            if hlpr_arr[i] == hlpr_arr[i - 1] and hlpr_arr[i]:
+                hlpr_arr[i] += 1
+                for x in range(i - 1, 0, -1):
+                    hlpr_arr[x] = hlpr_arr[x - 1]
+                hlpr_arr[0] = 0
+        for i in range(len(hlpr_arr)):
+            down_board[4 - len(hlpr_arr) + i][j] = hlpr_arr[i]
+    return down_board
+
+
 class Node:
     def __init__(self, board):
         self.board = board
         self.children = []
+
+    def create_children_from_player_moves(self):
+        print_board(create_up_board(self.board))
+        print_board(create_left_board(self.board))
+        print_board(create_right_board(self.board))
+        print_board(create_down_board(self.board))
+
+
 
 # The nodes which have the players turn next will have 4 children in the Order of UP,Right,Down,left
 # The nodes which have the games turn will have anywhere b/w 2-30 children. The children will be created by
@@ -24,12 +118,12 @@ class Node:
 
 
 class Tree:  # The tree will have the property that leaf nodes are always board states that have the players turn
-    def __init__(self, board):
-        self.node = Node(board)
+    def __init__(self, board, depth):
+        self.root = Node(board)
         assert(not lost(board)), "Cannot initialise a state-space tree with a board that has already lost"
+        assert (not depth % 2), "cannot initialise a tree with odd depth"
+        self.root.create_children_from_player_moves()
 
-    def increment_depth(self, node):
-        pass;
 
 
 def get_board(drive_window_):
@@ -41,6 +135,7 @@ def get_board(drive_window_):
             if element_list:
                 board_element = element_list[len(element_list)-1]
                 board[j - 1][i - 1] = math.floor(math.log2(int(board_element.text)))
+    print_board(board)
     return board
 
 
@@ -85,14 +180,15 @@ def lost(board):
 
 
 def play():
+    assert (not depth_of_expectimax % 2), "depth_of_expectimax must be even"
     with webdriver.Firefox() as driver:
         driver.get("https://2048game.com/")
         action = ActionChains(driver)
-        time.sleep(5)  # wait for the js to take effect and make the game appear
+        time.sleep(10)  # wait for the js to take effect and make the game appear
         board = get_board(driver)
-        state_space_tree = Tree(board)
-        while not lost(board):
-            pass
+        state_space_tree = Tree(board, depth_of_expectimax)
+        time.sleep(500)
+        # keep-playing-button , this is gonna be the class of the anchor tag , clicking on which will continue the game
 
 
 if __name__ != "__main__":
